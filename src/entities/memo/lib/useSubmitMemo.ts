616575555c31df3d79b24e1c9d77380ba4f4134a
zzzzzuuuuu <agu0830@naver.com>
2025-03-11 22:02:47 +0900

    feat: 메모 등록 훅 구현

--- src/entities/memo/lib/useSubmitMemo.ts
+++ src/entities/memo/lib/useSubmitMemo.ts
@@ -1,30 +1,14 @@
 import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { createMemo } from '@/shared/api/memo.ts';
+import { createMemo } from '@/shared/api/memo';
 
-export const useSubmitMemo = (
-  teamId: number,
-  title: string,
-  content: string,
-  tags: { name: string }[],
-  navigate: ReturnType<typeof useNavigate>,
-) => {
-  const handleSubmit = useCallback(async () => {
-    try {
+export const useSubmitMemo = (teamId: number) => {
+  const handleSubmit = useCallback(
+    async (title: string, content: string, tags: { name: string }[]) => {
       const tagNames = tags.map((tag) => tag.name);
-      const createMemoResult = await createMemo(
-        teamId,
-        title,
-        tagNames,
-        content,
-      );
-      console.log('메모: ', createMemoResult);
-      navigate(`/memo`);
-      console.log(title, tags, content);
-    } catch (error) {
-      console.error('메모 생성 오류: ', error);
-    }
-  }, [teamId, title, content, navigate]);
+      return await createMemo(teamId, title, tagNames, content);
+    },
+    [teamId],
+  );
 
   return { handleSubmit };
 };
