c664c0ae1632ed4d35c3c6292353fdc4b4103414
zzzzzuuuuu <agu0830@naver.com>
2025-03-18 21:28:03 +0900

    fix: 빌드 에러 수정

--- src/entities/memo/model/useSubmitMemo.ts
+++ src/entities/memo/model/useSubmitMemo.ts
@@ -1,11 +1,14 @@
 import { useCallback } from 'react';
-import { createMemo } from '@/shared/api/memo';
+// import { createMemo } from '@/shared/api/memo';
 
 export const useSubmitMemo = (teamId: number) => {
+  const isApi = false;
+
   const handleSubmit = useCallback(
     async (title: string, content: string, tags: { name: string }[]) => {
       const tagNames = tags.map((tag) => tag.name);
-      return await createMemo(teamId, title, tagNames, content);
+      if (isApi) console.log(teamId, title, tagNames, content);
+      // return await createMemo(teamId, title, tagNames, content);
     },
     [teamId],
   );
