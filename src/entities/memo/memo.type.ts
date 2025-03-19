279e958bf1bc1a3b786f0cffe7ab77f4ae80b187
zzzzzuuuuu <agu0830@naver.com>
2025-03-19 20:36:38 +0900

    feat: 메모, 폴더 삭제 모달 UI 구현

--- src/entities/memo/memo.type.ts
+++ src/entities/memo/memo.type.ts
@@ -8,3 +8,8 @@ export interface MemoFormProps {
   onDelete?: () => void; // EditMemo에서 사용
   showDeleteButton?: boolean;
 }
+
+export interface DeleteModalProps {
+  type: 'folder' | 'memo';
+  title: string;
+}
